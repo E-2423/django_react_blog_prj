61ed17f5173261e282edd3acd1ac7198c02dbd37
Joseph Abraham <ekurubal24@gmail.com>
2021-01-26 21:40:39 +0300

    Added Post

--- src/blogapp/models.py
+++ src/blogapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    options = (
+        ('d', 'Draft'),
+        ('p', 'Published')
+    )
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.URLField(max_length=5000, blank=True)
+    published_date = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=options, default='draft')
+    slug = models.SlugField(blank=True, unique=True)
+    
+    def __str__(self):
+        return self.title
+
