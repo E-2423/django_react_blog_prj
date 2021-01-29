540d6b972a5adf51f42ff7be87f194d27f3c0fc4
Joseph Abraham <ekurubal24@gmail.com>
2021-01-29 18:57:58 +0300

    added register

--- src/user_api/serializers.py
+++ src/user_api/serializers.py
@@ -23,14 +23,32 @@ class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
         write_only=True, required=True, validators=[validate_password], style={'input_type':'password'}
     )
-    password2 = serializers.CharField(
-        write_only=True, required=True,  style={'input_type':'password'}
-    )
+    password2 = serializers.CharField(write_only=True, required=True,  style={
+                                    'input_type':'password'})
     
-    model = User
-    fields = (
-        "username",
-        "email",
-        "password",
-        "password2",
-    )
+    
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "email",
+            "password",
+            "password2",
+        )
+        
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match"}
+            )
+        return attrs
+    
+    def create(self, validated_data):
+        user =User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        
+        return user
